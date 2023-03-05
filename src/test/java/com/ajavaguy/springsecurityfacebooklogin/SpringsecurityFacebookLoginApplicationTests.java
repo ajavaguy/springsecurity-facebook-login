30dd50437c9b6420307e53ea261b1a5c74bf9e7a
ajavaguy <ajavaguy96@gmail.com>
2023-03-05 17:27:47 +0700

    Login by facebook or github using OAuth2 demo

---
+++ src/test/java/com/ajavaguy/springsecurityfacebooklogin/SpringsecurityFacebookLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ajavaguy.springsecurityfacebooklogin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringsecurityFacebookLoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
